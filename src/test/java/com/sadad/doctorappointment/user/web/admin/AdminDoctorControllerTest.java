850b927a6f2b0a7e0588109968279d46338ce80d
Shayan <nosrat.mirzaee@gmail.com>
2024-09-13 11:31:49 +0330

    add test

--- src/test/java/com/sadad/doctorappointment/user/web/admin/AdminDoctorControllerTest.java
+++ src/test/java/com/sadad/doctorappointment/user/web/admin/AdminDoctorControllerTest.java
@@ -21,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
 @Log4j2
 public class AdminDoctorControllerTest {
